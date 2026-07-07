9b25324b79b67a7593b0968a04a2ffd0d77b83b4
nolight132 <contact@nolight.dev>
2026-07-08 00:08:19 +0200

    chore: update nls to v0.5.0

--- Formula/nls.rb
+++ Formula/nls.rb
@@ -1,8 +1,8 @@
 class Nls < Formula
   desc "Nushell-style ls for bash, zsh, and fish"
   homepage "https://github.com/nolight132/nls"
-  url "https://github.com/nolight132/nls/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "5b50169336aa1867e36c7bc10b96a05f311d0a713573aba523a1701255468d13"
+  url "https://github.com/nolight132/nls/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "14d0d87e12ac681234d400cc21839309dec622e9114a580b99d9678d004487dd"
   license "MIT"
 
   depends_on "go" => :build
